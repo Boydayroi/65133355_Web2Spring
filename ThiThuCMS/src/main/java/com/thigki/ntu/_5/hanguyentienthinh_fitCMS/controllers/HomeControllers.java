714a7819d8bd9f7fdfa2ae90aeb277c847a6af7c
Boydayroi <hanguyentienthinh@gmail.com>
2026-03-11 16:22:10 +0700

    anh

--- ThiThuCMS/src/main/java/com/thigki/ntu/_5/hanguyentienthinh_fitCMS/controllers/HomeControllers.java
+++ ThiThuCMS/src/main/java/com/thigki/ntu/_5/hanguyentienthinh_fitCMS/controllers/HomeControllers.java
@@ -1,13 +1,45 @@
 package com.thigki.ntu._5.hanguyentienthinh_fitCMS.controllers;
 
-import org.springframework.stereotype.Controller;
+import java.util.ArrayList;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model; 
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.thigki.ntu._5.hanguyentienthinh_fitCMS.models.Page;
+import com.thigki.ntu._5.hanguyentienthinh_fitCMS.models.Post;
+
 @Controller
 public class HomeControllers {
+	
+	ArrayList<Page> dsTrang = new ArrayList<Page>();
+	ArrayList<Post> dsBaiViet = new ArrayList<Post>();
+	
+	
+	public HomeControllers() {
+		
+		dsTrang.add(new Page(1, "Trang chủ", "home", "Nội dung trang chủ", 0));
+		dsTrang.add(new Page(2, "Giới thiệu", "about", "Giới thiệu về CMS", 0));
+		dsTrang.add(new Page(3, "Liên hệ", "contact", "Thông tin liên hệ", 0));
+		
+		
+		dsBaiViet.add(new Post(1, "Học Spring Boot", "Nội dung bài học Spring Boot", 1, "spring.png"));
+		dsBaiViet.add(new Post(2, "Lập trình Web", "Hướng dẫn làm web cơ bản", 1, "web.png"));
+		dsBaiViet.add(new Post(3, "Kinh nghiệm IT", "Chia sẻ kinh nghiệm học tập", 2, "it.png"));
+	}
+
+	
 	@GetMapping("/")
 	public String home() {
-		return "Index";
+		return "Index"; 
+	}
+		
+	@GetMapping("/Bai2")
+	public String bai2(Model model) { 
+		
+		model.addAttribute("dsTrang", dsTrang);
+		model.addAttribute("dsBaiViet", dsBaiViet);
+		
+		return "Bai2"; 
 	}
 }
-
