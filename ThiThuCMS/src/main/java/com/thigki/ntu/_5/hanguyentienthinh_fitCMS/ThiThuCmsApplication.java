80df50d8355b4f9c422cd79f26ab12d3bae341aa
Boydayroi <hanguyentienthinh@gmail.com>
2026-03-11 14:37:15 +0700

    Thigki

---
+++ ThiThuCMS/src/main/java/com/thigki/ntu/_5/hanguyentienthinh_fitCMS/ThiThuCmsApplication.java
@@ -0,0 +1,13 @@
+package com.thigki.ntu._5.hanguyentienthinh_fitCMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThiThuCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThiThuCmsApplication.class, args);
+	}
+
+}
---
+++ ThiThuCMS/src/test/java/com/thigki/ntu/_5/hanguyentienthinh_fitCMS/ThiThuCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thigki.ntu._5.hanguyentienthinh_fitCMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThiThuCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
