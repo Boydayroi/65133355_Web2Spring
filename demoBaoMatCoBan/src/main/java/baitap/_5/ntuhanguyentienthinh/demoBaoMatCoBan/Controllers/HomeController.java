9de9e3a08aa48485a8e9b3c64fb505773cbc9cd6
Boydayroi <hanguyentienthinh@gmail.com>
2026-03-25 15:33:06 +0700

    demo

--- demoBaoMatCoBan/src/main/java/baitap/_5/ntuhanguyentienthinh/demoBaoMatCoBan/Controllers/HomeController.java
+++ demoBaoMatCoBan/src/main/java/baitap/_5/ntuhanguyentienthinh/demoBaoMatCoBan/Controllers/HomeController.java
@@ -1,10 +1,25 @@
 package baitap._5.ntuhanguyentienthinh.demoBaoMatCoBan.Controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+@Controller
 public class HomeController {
 @GetMapping("/")
 public String index() {
 	return "index";
 }
+@GetMapping("/products/")
+public String index1() {
+	return "sanpham";
 }
+@GetMapping("/admincp")
+public String index2() {
+	return "indexadmin";
+}
+@GetMapping("/hello")
+public String hi() {
+	return "hello";
+}
+
+}
+
