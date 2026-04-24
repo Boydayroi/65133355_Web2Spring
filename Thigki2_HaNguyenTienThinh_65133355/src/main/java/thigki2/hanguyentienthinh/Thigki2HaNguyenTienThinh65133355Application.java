66bc715aafb6d5550fb8d1c987d7b68475df9e53
Boydayroi <hanguyentienthinh@gmail.com>
2026-04-24 13:42:14 +0700

    lần thứ nhất

---
+++ Thigki2_HaNguyenTienThinh_65133355/src/main/java/thigki2/hanguyentienthinh/Thigki2HaNguyenTienThinh65133355Application.java
@@ -0,0 +1,13 @@
+package thigki2.hanguyentienthinh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Thigki2HaNguyenTienThinh65133355Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Thigki2HaNguyenTienThinh65133355Application.class, args);
+	}
+
+}
---
+++ Thigki2_HaNguyenTienThinh_65133355/src/test/java/thigki2/hanguyentienthinh/Thigki2HaNguyenTienThinh65133355ApplicationTests.java
@@ -0,0 +1,13 @@
+package thigki2.hanguyentienthinh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Thigki2HaNguyenTienThinh65133355ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
